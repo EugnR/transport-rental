87e712d417e86057d67a658dc152b6236a73f5ed
EugnR <sahibgareevgohn@gmail.com>
2025-07-13 01:34:04 +0400

    - Заменил внедрение TransportServiceImpl на TransportService. - Вынес логику обработки исключений из контроллера в класс для обработчиков

--- src/main/java/ru/transport/rent/controller/TransportController.java
+++ src/main/java/ru/transport/rent/controller/TransportController.java
@@ -1,16 +1,13 @@
 package ru.transport.rent.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.transport.rent.dto.transport.RequestRegisterTransportDTO;
-import ru.transport.rent.exceptions.InvalidTransportTypeException;
-import ru.transport.rent.service.transport.TransportServiceImpl;
+import ru.transport.rent.service.transport.TransportService;
 
 /**
  * Контроллер для запросов на действия с транспортом.
@@ -20,20 +17,14 @@ import ru.transport.rent.service.transport.TransportServiceImpl;
 @RequestMapping("/api/Transport")
 public class TransportController {
 
-    private final TransportServiceImpl transportServiceImpl;
+    private final TransportService transportService;
 
     /**
      * Метод для регистрации нового транспорта.
      */
     @PostMapping
     public ResponseEntity<?> registerTransport(@RequestBody final RequestRegisterTransportDTO registerTransportDTO) {
-        try {
-            transportServiceImpl.registerTransport(registerTransportDTO);
-            return ResponseEntity.ok().build();
-        } catch (UsernameNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
-        } catch (InvalidTransportTypeException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        }
+        transportService.registerTransport(registerTransportDTO);
+        return ResponseEntity.ok().build();
     }
 }
